76f93d8fd44a2b4ac4dc5a3179dfcf9d79b9f212
jinny232 <129250094+jinny232@users.noreply.github.com>
2026-06-22 19:00:00 +0630

    Delete app/admin/team-assignments directory

--- app/admin/team-assignments/page.tsx
+++
@@ -1,5 +0,0 @@
-import TeamAssignmentsClient from "@/components/admin/team-assignments/TeamAssignmentsClient";
-
-export default function AdminTeamAssignmentsPage() {
-  return <TeamAssignmentsClient />;
-}
